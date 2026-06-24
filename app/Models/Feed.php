1a37d37c00fb7e02614a8fca81d19850c7cce346
241230061-debug <241230061@unmuhpnk.ac.id>
2026-06-24 11:56:09 +0700

    perbaikan bug

--- app/Models/Feed.php
+++ app/Models/Feed.php
@@ -3,7 +3,8 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-use illuminate\Database\Eloquent\Factories\HasFactory;
+// Change 'illuminate' to 'Illuminate' below:
+use Illuminate\Database\Eloquent\Factories\HasFactory; 
 
 class Feed extends Model
 {
